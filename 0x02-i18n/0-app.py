e5bc162f4a8f8053db8b339a76e60ab0c3b00dbc
jobs-rw <mjabush@yahoo.fr>
2024-02-06 18:32:41 +0000

    commit on updates for question 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,13 +2,20 @@
 """
 A Basic flask application
 """
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html', title='Welcome to Holberton', header='Hello world')
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Renders a basic html template
+    """
+    return render_template('0-index.html')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
